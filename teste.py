b3d5738e2ae80be38bf1cac6195c0eed043d6f00
Angela-Leao <ammlpotter934@gmail.com>
2026-03-02 12:29:50 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,3 +2,13 @@ print("Olá, mundo!")
 input("Como está o dia?")
 print("Parece bom pra aprender a programar")
       
+selva=('formiga,onça,anta') 
+oceano=('peixe,baleia')
+caatinga=('seca,lobo_guará,calor')
+cerrado=(selva + ',' + caatinga)
+    
+print(selva)
+print(oceano)
+print(caatinga)
+print(cerrado)
+print(type(oceano))
